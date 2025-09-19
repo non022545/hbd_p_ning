9774fbf8f02fee7509269223a72a869da7258095
non022545 <nontthont7@gmail.com>
2025-09-19 22:20:11 +0700

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <>
+            <div className="min-h-screen w-full py-4 px-2 bg-gradient-to-br from-purple-400 via-pink-500 to-pink-300 bg-[length:200%_200%] animate-gradient">
+                <div className='bg-white/60 text-center text-xl text-pink-900 p-2 rounded-full font-bold'>&#129392; HAPPY BIRTHDAY TO P.NING &#129392;</div>
+                <div>
+                    ขอให้พี่มีความสุขมากๆล่างกายแข็งแรงงง
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        gradient: {
+          "0%, 100%": { "background-position": "0% 50%" },
+          "50%": { "background-position": "100% 50%" },
+        },
+      },
+      animation: {
+        gradient: "gradient 6s ease infinite",
+      },
+    },
+  },
+  plugins: [],
+}
