4ec8b1fb979f11764a73706aa191069c40685c4a
non022545 <nontthont7@gmail.com>
2025-09-20 21:50:22 +0700

    dasd

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -52,7 +52,7 @@ function Home() {
       <div className="flex flex-col justify-center min-h-screen w-full py-4 px-2 bg-gradient-to-br from-purple-400 via-pink-500 to-pink-300 bg-[length:200%_200%] animate-gradient">
         <div className="bg-white/50 p-2 rounded-3xl backdrop-blur-sm">
           <div className="bg-white/60 text-center text-xl text-pink-900 p-2 rounded-full font-bold">
-            &#129392; HAPPY BIRTHDAY TO P.NING &#129392;
+            &#129392; HAPPY BIRTHDAY TO P'NING &#129392;
           </div>
 
           <div className="flex justify-center">
