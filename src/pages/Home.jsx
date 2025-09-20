6440eef54ee4936d8a106f4caaa3ba2c6e5ee1d5
non022545 <nontthont7@gmail.com>
2025-09-20 21:54:39 +0700

    dasdsa

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -49,10 +49,10 @@ function Home() {
 
   return (
     <>
-      <div className="flex flex-col justify-center min-h-screen w-full py-4 px-2 bg-gradient-to-br from-purple-400 via-pink-500 to-pink-300 bg-[length:200%_200%] animate-gradient">
-        <div className="bg-white/50 p-2 rounded-3xl backdrop-blur-sm">
-          <div className="bg-white/60 text-center text-xl text-pink-900 p-2 rounded-full font-bold">
-            &#129392; HAPPY BIRTHDAY TO P'NING &#129392;
+      <div className="flex flex-col justify-center min-h-screen w-full py-4 px-2  bg-gradient-to-br from-purple-400 via-pink-500 to-pink-300 bg-[length:200%_200%] animate-gradient">
+        <div className="bg-white/50 px-2 py-4 rounded-3xl backdrop-blur-sm">
+          <div className="bg-white/60 text-center text-xl text-pink-900 p-2 rounded-3xl font-bold">
+            &#129392; HAPPY BIRTHDAY &#129392; <br /> &#128150; P'NING &#128150;
           </div>
 
           <div className="flex justify-center">
